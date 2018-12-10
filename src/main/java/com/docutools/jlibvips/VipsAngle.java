70acb368e66b13ec03d96bfb3bc9ecc3e90e9b1c
amp <a.partsch@docu-tools.com>
2018-12-10 09:39:37 +0100

    Add fromInteger Creation Method for VipsAngle

--- src/main/java/com/docutools/jlibvips/VipsAngle.java
+++ src/main/java/com/docutools/jlibvips/VipsAngle.java
@@ -11,5 +11,23 @@ public enum VipsAngle {
     D90,
     D180,
     D270,
-    Last
+    Last;
+
+    /**
+     * Creates a {@link VipsAngle} based on degrees as integer.
+     *
+     * @param value degrees
+     * @return the {@link VipsAngle}
+     * @throws IllegalArgumentException when angle is not 0, 90, 180 or 270.
+     */
+    public static VipsAngle fromInteger(int value) {
+        switch (value) {
+            case 0: return D0;
+            case 90: return D90;
+            case 180: return D180;
+            case 270: return D270;
+            default:
+                throw new IllegalArgumentException("Allowed VipsAngle's are [0°, 90°, 180°, 270°].");
+        }
+    }
 }
